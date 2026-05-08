0689edd6f4d71b62dfa2257b98923499fff2098a
Nguyễn Huỳnh Khánh <khanhvps.mmo@gmail.com>
2026-05-08 10:37:11 +0700

    Fix: Khắc phục lỗi nháy UI và tinh chỉnh giao diện" -m "- Gỡ bỏ event listener toàn cục gây xung đột, sử dụng @click.outside chuẩn của Alpine.js để quản lý đóng/mở menu. - Thêm x-cloak để chống lỗi chớp nháy (flash) Modal Bộ lọc và Dropdown khi tải trang. - Xử lý triệt để lỗi click xuyên thấu (synthetic click) trên Mobile bằng @click.prevent.stop. - Xóa icon ở nút 'Bộ lọc' và link 'Xem tất cả' để giao diện gọn gàng hơn.

--- resources/views/trang-chu.blade.php
+++ resources/views/trang-chu.blade.php
@@ -115,7 +115,6 @@
             </div>
             <a href="{{ route('truyen.danh-sach', ['sap_xep' => 'luot_xem']) }}" class="btn-quiet">
                 Xem tất cả
-                <svg class="h-4 w-4 transition-transform group-hover:translate-x-1" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 8l4 4m0 0l-4-4m4 4H3"/></svg>
             </a>
         </div>
 
--- resources/views/truyen/danh-sach.blade.php
+++ resources/views/truyen/danh-sach.blade.php
@@ -124,7 +124,7 @@
                 
                 {{-- Mobile Filter Trigger --}}
                 <button @click.prevent.stop="openFilters = true" class="btn-secondary lg:hidden text-xs">
-                    <svg class="h-4 w-4" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6V4m0 2a2 2 0 100 4m0-4a2 2 0 110 4m-6 8a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4m6 6v10m6-2a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4"/></svg>
+                    
                     Bộ lọc
                 </button>
             </div>
